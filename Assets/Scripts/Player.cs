6e635bfd79355a821eedaf477be359c202c3ab45
Noah Laville <noahlaville14@gmail.com>
2018-12-10 17:13:24 -0500

    Player
    Created player object

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float verticalVelocity;
+
+    CharacterController cc;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Setting up move position
+        Vector3 movePosition = Vector3.zero;
+        movePosition.x = speed;
+        movePosition.y -= 9.8f;
+        cc.Move(movePosition * Time.deltaTime);
+
+
+	}
+}
